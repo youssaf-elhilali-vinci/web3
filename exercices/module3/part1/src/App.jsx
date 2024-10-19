46352ac76319d1e051895bc5de238e92dd9943e3
El hilali <youssaf.elhilali@student.vinci.be>
2024-10-19 16:01:35 +0200

    module3: loading

--- exercices/module3/part1/src/App.jsx
+++ exercices/module3/part1/src/App.jsx
@@ -1,51 +1,82 @@
 import { useState } from 'react'
 import './App.css'
 
+
+function StatisticLine({ text, value }) {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>{text}</td>
+          <td>{value}</td>
+        </tr>
+      </tbody>
+    </table>
+
+  )
+}
+
+function Statistics({ countGood, countNeutral, countBad, countAll }) {
+  return (
+    <>
+      {(countAll === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <div>
+          <h1>Statistics</h1>
+          <StatisticLine text="good" value={countGood} />
+          <StatisticLine text="neutral" value={countNeutral} />
+          <StatisticLine text="bad" value={countBad} />
+          <StatisticLine text="all" value={countAll} />
+          <StatisticLine text="average" value={(countGood - countBad) / countAll} />
+          <StatisticLine text="positive" value={(countGood / countAll) * 100 + "%"} />
+        </div>
+      )
+      )}
+    </>
+
+  )
+}
+
+
+
 function App() {
   const [countGood, setCountGood] = useState(0)
   const [countNeutral, setCountNeutral] = useState(0)
   const [countBad, setCountBad] = useState(0)
   const [countAll, setCountAll] = useState(0)
 
+
+  const handleGood = () => {
+    setCountGood(countGood + 1)
+    setCountAll(countAll + 1)
+  }
+
+  const handleNeutral = () => {
+    setCountNeutral(countNeutral + 1)
+    setCountAll(countAll + 1)
+  }
+
+  const handleBad = () => {
+    setCountBad(countBad + 1)
+    setCountAll(countAll + 1)
+  }
+
   return (
     <>
-    <div>
-      <h1>Give feedback</h1>
-      <button onClick={() => setCountGood(countGood + 1) + setCountAll(countAll + 1)}>Good</button>
-      <button onClick={() => setCountNeutral(countNeutral + 1) + setCountAll(countAll + 1)}>Neutral</button>
-      <button onClick={() => setCountBad(countBad + 1) + setCountAll(countAll + 1)}>Bad</button>
-
-    </div>
-    
-    {(countAll === 0 ? (
-      <p>No feedback given</p>
-    ) : (
       <div>
-        <h1>Statistics</h1>
-        <p>good {countGood}</p>
-        <p>neutral {countNeutral}</p>
-        <p>bad {countBad}</p>
-        <p>all {countAll}</p>
-        <p>average {(countGood - countBad) / countAll}</p>
-        <p> positive {(countGood / countAll) * 100}%</p>
+        <h1>Give feedback</h1>
+        <button onClick={handleGood}>Good</button>
+        <button onClick={handleNeutral}>Neutral</button>
+        <button onClick={handleBad}>Bad</button>
+
       </div>
-    )
-    )}
 
+      <Statistics countGood={countGood} countNeutral={countNeutral} countBad={countBad} countAll={countAll} />
 
-
-
-
-
-    
     </>
   )
 
-
-
-
 }
-
-
 
 export default App
